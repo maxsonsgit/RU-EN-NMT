2366e1851ad32bc786eb442ccf0b64a9c8ff5419
maxsonsgit <son.mp146@gmail.com>
2026-09-13 00:38:22 +0300

    fix the unsaved verison of 01_ib_model.py

--- src/ru_en_nmt/models/01_ibm_model1.py
+++ src/ru_en_nmt/models/01_ibm_model1.py
@@ -5,6 +5,9 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 
+from evaluation import bleu
+
+
 def tokenize(text: str, lang: str):
     if lang == "ru":
         tokens = tokenizer_ru.tokenize(text)
@@ -59,21 +62,7 @@ en_sentense = detoketizer.detokenize(translotion_sample)
 hypotheses = [translate(sentence, model) for sentence in X_test_token]
 references = [[ref] for ref in y_test_token]
 
-
-from nltk.translate.bleu_score import corpus_bleu, SmoothingFunction
-
-
-def bleu(references, hypotheses):
-    bleu = corpus_bleu(
-        references, 
-        hypotheses,
-        weights=(0.25, 0.25, 0.25, 0.25),
-        smoothing_function=SmoothingFunction().method4
-        )
-
-    return bleu * 100
-
 print()
 print("-" * 100)
-print(bleu(references, hypotheses))
+print(f"{bleu(references, hypotheses):.4f}")
 print("-" * 100)
